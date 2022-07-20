955cd1442029dc1def968ac3df509aeff6d6552b
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-07-20 15:10:35 +0300

    Task 10.7 completion

--- rate-repository-app/components/AppBar.jsx
+++ rate-repository-app/components/AppBar.jsx
@@ -1,23 +1,27 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flexDirection: 'row',
     backgroundColor: "#24292e", 
     paddingTop: Constants.statusBarHeight,
     paddingBottom: 10
   },
+  scroll: {
+    flexDirection: 'row',
+  }
 });
 
 const AppBar = () => {
   //<AppBarTab url="/signin" tabName="Sign in"/>
   return ( 
     <View style={styles.container}>
+      <ScrollView styles={styles.scroll} horizontal>
         <AppBarTab url="/" tabName="Repositories"/>
         <AppBarTab url="/signin" tabName="Sign in"/>
+      </ScrollView>
     </View>
   );
 };
