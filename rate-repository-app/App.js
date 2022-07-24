f93b36facff071f6f8d612642bed561bc593ae66
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-07-24 13:00:23 +0300

    Task 10.12 completion

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,10 +3,13 @@ import createApolloClient from "./utils/apolloClient";
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
+import Constants from "expo-constants";
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
+  console.log(Constants.manifest.extra.uri);
   return (
     <>
       <NativeRouter>
